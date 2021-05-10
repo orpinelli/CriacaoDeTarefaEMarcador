f8b08ec9f279d5a5d06067788bff81f611575faa
Gabriel Orpinelli <68570239+orpinelli@users.noreply.github.com>
2021-05-10 15:22:50 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,81 @@
+import userEvent from '@testing-library/user-event';
+import React, { useState, useEffect} from 'react';
+import styled from 'styled-components';
+ import SearchBox from './components/SearchBox';
+
+
+
+function App() {
+
+  const [serachtext, setSearchtext] = useState ('');
+
+  const [lista, setLista] = useState([]);
+
+  useEffect (() => {
+
+    setLista([
+      {title:'Comrpar o bolo', done:false},
+      {title:'garvar aula', done:true},
+      {title:'Estuda', done:true}
+      
+    ])
+  }, []);
+
+  function addAction (newItem) {
+    
+      let newLista = [...lista, {title:newItem, done:false}];
+      setLista(newLista);
+
+  }
+
+  function handleToggDone(index) {
+    let newLista = [...lista];
+    newLista[index].done = !newLista[index].done;
+
+    setLista(newLista);
+
+  }
+
+
+
+  return (
+    <>
+       
+    <h1> Lista de tarefas</h1>
+
+      <SearchBox frasePadrao = "Cadastra item na lista: "
+      onEnter = {addAction}
+ 
+        
+      />
+    <hr/>
+
+<ul>
+      {lista.map((item, index) => (
+
+          <li key ={index}>
+           {item.done &&
+            <del> {item.title} </del>
+           }
+
+           {!item.done &&
+            item.title
+           }
+           <button onClick={()=>handleToggDone(index)}>
+             
+              {item.done && 'Desfazer'}
+              {!item.done && 'fazer'}
+
+            </button>
+            
+             </li>
+
+      ) )}
+      
+</ul>
+
+    </>
+  );
+}
+
+export default App;
---
+++ SearchBox.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
+
+
+const InputText = styled.input`
+
+    border:2px solid #000;
+    border-radius:5px;
+    padding:15px;
+`;
+function SearchBox(props) {
+
+        const [texto, setTexto] = useState('');
+
+
+
+        function handleKeyUp (e){
+           if(e.keyCode == 13){
+                    if (props.onEnter) {
+                        props.onEnter (texto);
+                    }
+                    setTexto ('');
+                }
+            }
+
+    return(
+
+        <InputText 
+        type="text" 
+        value={texto} 
+        onChange= {(e) => setTexto (e.target.value)} 
+        onKeyUp = {handleKeyUp}
+        placeholder={props.frasePadrao ?? "Digite" }
+        />
+        
+
+    );
+}
+
+export default SearchBox;
